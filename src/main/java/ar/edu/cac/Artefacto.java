13cab918f2d205c38837933dac8a888b463a2807
Jeboy <tovilajesus@gmail.com>
2023-04-13 20:35:53 -0300

    first commit

---
+++ src/main/java/ar/edu/cac/Artefacto.java
@@ -0,0 +1,4 @@
+package ar.edu.cac;
+
+public class Artefacto {
+}
---
+++ src/main/java/ar/edu/cac/Guerrero.java
@@ -0,0 +1,40 @@
+package ar.edu.cac;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Guerrero {
+
+    private Integer nivelDeLucha;
+
+    private List<Artefacto> artefactos;
+
+    public Guerrero(){
+        artefactos = new ArrayList<>();
+        nivelDeLucha = 0;
+    }
+
+    public Integer getNivelDeLucha() {
+        return nivelDeLucha;
+    }
+
+    public void setNivelDeLucha(Integer nivelDeLucha) {
+        this.nivelDeLucha = nivelDeLucha;
+    }
+
+    public List<Artefacto> getArtefactos() {
+        return artefactos;
+    }
+
+    public void setArtefactos(List<Artefacto> artefactos) {
+        this.artefactos = artefactos;
+    }
+
+    public void agregarArtefacto(Artefacto artefacto) {
+        artefactos.add(artefacto);
+    }
+
+    public Integer getCantidadArtefactos() {
+        return artefactos.size();
+    }
+}
